0c6b0edb1f68e48261d30f948e7e8027e77ba3c5
cristian.ramirez@harness.io <cristiano.rosetti@gmail.com>
2024-08-04 15:55:03 -0600

    feat: update static files

--- output.tf
+++ output.tf
@@ -4,3 +4,7 @@ output "website_url" {
   value = "http://${aws_s3_bucket.my-static-website.bucket}.s3-website.${var.region}.amazonaws.com"
 }
 
+output "bucket_name" {
+  value = var.bucket_name
+}
+
--- templates/static-website/output.tf
+++ templates/static-website/output.tf
@@ -1,6 +1,8 @@
-# s3 static website url
-
 output "website_url" {
   value = module.terraform-aws-s3.website_url
+}
+
+output "bucket_name" {
+  value = module.terraform-aws-s3.bucket_name
 }
 
